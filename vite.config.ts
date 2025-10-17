157e5e97684663c7783731be391c1ae95a49acfb
mohiramansurovna <mohiramansurovna07@gmail.com>
2025-10-17 23:36:23 +0500

    fixed imports from .env

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Use BASE_PATH env (set in CI) to correctly serve under /<repo>/ on GitHub Pages
-const base = process.env.BASE_PATH || '/';
+const base = import.meta.env.BASE_PATH || '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
